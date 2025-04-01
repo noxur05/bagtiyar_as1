8a3d92e70d637d75748c21e1560155cb0dcbbd67
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-01 22:22:56 +0500

    updated restaurant and city tables

--- database/migrations/2025_04_01_164747_create_restaurants_table.php
+++ database/migrations/2025_04_01_164747_create_restaurants_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cities', function (Blueprint $table) {
+        Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
         });
     }
 
@@ -22,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cities');
+        Schema::dropIfExists('restaurants');
     }
 };
--- database/migrations/2025_04_01_171424_create_categories_table.php
+++ database/migrations/2025_04_01_171424_create_categories_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cities', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 255);
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cities');
+        Schema::dropIfExists('categories');
     }
 };
